c9795c06e416c17cdcbe724221b7323aa67627eb
timofei <polozovt@yandex.ru>
2026-03-20 20:10:41 +0300

    feat: removed fluent sdk package and updated docker scripts

---
+++ configs/app/fluent.ts
@@ -0,0 +1,44 @@
+export const DEVNET_NETWORK = 'fluent';
+export const DEVNET_NETWORK_NATIVE_CURRENCY = {
+  name: 'Ether',
+  symbol: 'ETH',
+  decimals: 18,
+} as const;
+export const DEVNET_EXPLORER_NAME = 'Fluent Block Explorer';
+export const FLUENT_DEVNET_CHAIN_ID = 20993;
+export const DEVNET_EXPLORER_HOST = 'devnet.fluentscan.xyz';
+export const DEVNET_EXPLORER_URL = `https://${ DEVNET_EXPLORER_HOST }`;
+export const DEVNET_EXPLORER_API_HOST = 'api-devnet.fluentscan.xyz';
+export const DEVNET_EXPLORER_API_URL = `https://${ DEVNET_EXPLORER_API_HOST }`;
+export const DEVNET_RPC_URL = 'https://rpc.dev.gblend.xyz';
+export const DEVNET_NETWORK_NAME = 'Fluent Developer Preview';
+
+export const MAINNET_NETWORK = 'fluent';
+export const MAINNET_NETWORK_NATIVE_CURRENCY = {
+  name: 'Ether',
+  symbol: 'ETH',
+  decimals: 18,
+} as const;
+export const MAINNET_EXPLORER_NAME = 'Fluent Block Explorer';
+export const FLUENT_MAINNET_CHAIN_ID = 25363;
+export const MAINNET_EXPLORER_HOST = 'fluentscan.xyz';
+export const MAINNET_EXPLORER_URL = `https://${ MAINNET_EXPLORER_HOST }`;
+export const MAINNET_EXPLORER_API_HOST = 'api.fluentscan.xyz';
+export const MAINNET_EXPLORER_API_URL = `https://${ MAINNET_EXPLORER_API_HOST }`;
+export const MAINNET_RPC_URL = 'https://rpc.fluent.xyz';
+export const MAINNET_NETWORK_NAME = 'Fluent Mainnet';
+
+export const TESTNET_NETWORK = 'fluent';
+export const TESTNET_NETWORK_NATIVE_CURRENCY = {
+  name: 'Ether',
+  symbol: 'ETH',
+  decimals: 18,
+} as const;
+export const TESTNET_EXPLORER_NAME = 'Fluent Block Explorer';
+export const FLUENT_TESTNET_CHAIN_ID = 20994;
+export const TESTNET_EXPLORER_HOST = 'testnet.fluentscan.xyz';
+export const TESTNET_EXPLORER_URL = `https://${ TESTNET_EXPLORER_HOST }`;
+export const TESTNET_EXPLORER_API_HOST = 'api-testnet.fluentscan.xyz';
+export const TESTNET_EXPLORER_API_URL = `https://${ TESTNET_EXPLORER_API_HOST }`;
+export const TESTNET_RPC_URL = 'https://rpc.testnet.fluent.xyz';
+export const TESTNET_NETWORK_NAME = 'Fluent Testnet';
