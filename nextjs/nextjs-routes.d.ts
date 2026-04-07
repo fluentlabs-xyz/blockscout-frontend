0e54b23d3a8e9677a79df6c6587f5765be94ae7b
timofei <polozovt@yandex.ru>
2026-04-07 21:22:19 +0300

    fix: returned routes

--- nextjs/nextjs-routes.d.ts
+++ nextjs/nextjs-routes.d.ts
@@ -46,6 +46,7 @@ declare module "nextjs-routes" {
     | DynamicRoute<"/chain/[chain_slug]/block/countdown", { "chain_slug": string }>
     | DynamicRoute<"/chain/[chain_slug]/csv-export", { "chain_slug": string }>
     | DynamicRoute<"/chain/[chain_slug]/op/[hash]", { "chain_slug": string; "hash": string }>
+    | DynamicRoute<"/chain/[chain_slug]/runtime-upgrades", { "chain_slug": string }>
     | DynamicRoute<"/chain/[chain_slug]/token/[hash]", { "chain_slug": string; "hash": string }>
     | DynamicRoute<"/chain/[chain_slug]/token/[hash]/instance/[id]", { "chain_slug": string; "hash": string; "id": string }>
     | DynamicRoute<"/chain/[chain_slug]/tx/[hash]", { "chain_slug": string; "hash": string }>
